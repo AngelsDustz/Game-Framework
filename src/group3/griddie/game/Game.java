370bca27e3fd23f378b59da98e92cc9c2add22b3
Faimire <faimireg@hotmail.com>
2019-04-05 10:22:01 +0200

    stash

--- src/group3/griddie/game/Game.java
+++ src/group3/griddie/game/Game.java
@@ -1,7 +1,6 @@
 package group3.griddie.game;
 
 import group3.griddie.controller.board.BoardController;
-import group3.griddie.game.player.ai_test_player_jonathan;
 import group3.griddie.model.board.Board;
 import group3.griddie.game.player.HumanPlayer;
 import group3.griddie.game.player.Player;
@@ -25,7 +24,6 @@ public abstract class Game extends Scene {
 
         //JUST FOR TESTING
         addPlayer(new HumanPlayer(this, "Player 1"));
-        addPlayer(new ai_test_player_jonathan(this, "Player 2"));
     }
 
     public final void init() {
